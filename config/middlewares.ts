623be753f4ed04c3810c784d9f60cd31ee5ec229
Islam Elmokattaf <islam@Islams-Mac-mini.local>
2026-04-02 18:14:11 +0300

    update cors domains

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,12 +8,12 @@ export default [
       enabled: true,
       headers: "*",
       origin: [
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        // Add your Vercel domains:
-        // "https://your-project.vercel.app",
-        // "https://yourdomain.com",
-      ],
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://islam-24-frontend.vercel.app",
+      "https://islam-24.com",
+      "https://www.islam-24.com"
+    ],
     },
   },
   "strapi::poweredBy",
