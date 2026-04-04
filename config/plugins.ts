e9d1cc528b2b09eaa63ed605e2624bb43f850715
Islam <islam.mapanalyst@gmail.com>
2026-04-04 03:46:11 +0300

    Connect Strapi to Cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,18 @@
-export default () => ({
+export default ({ env }: any) => ({
   upload: {
     config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      // الـ breakpoints لقص الصور تلقائياً لتناسب جميع الشاشات
       breakpoints: {
         xlarge: 1920,
         large: 1000,
