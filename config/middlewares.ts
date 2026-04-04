cffe2819741fc69ab876d1f396fcb64ea6c10d97
Islam <islam.mapanalyst@gmail.com>
2026-04-04 05:06:06 +0300

    allow cloudinary images in CSP

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,41 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+          ],
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
       enabled: true,
       headers: "*",
       origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://islam-24-frontend.vercel.app",
-      "https://islam-24.com",
-      "https://www.islam-24.com"
-    ],
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://islam-24-frontend.vercel.app",
+        "https://islam-24.com",
+        "https://www.islam-24.com",
+      ],
     },
   },
   "strapi::poweredBy",
